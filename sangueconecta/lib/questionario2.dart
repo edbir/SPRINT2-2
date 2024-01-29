e9f406fd74b8196d1e211c07b5291dfeabc40acf
Lucas Lopes <lopeslucas855@gmail.com>
2024-01-29 18:25:51 -0300

    Questionario

--- sangueconecta/lib/questionario2.dart
+++ sangueconecta/lib/questionario2.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'questionario3.dart';
+import 'questionario4.dart';
 
 class Questionario2Screen extends StatefulWidget {
   @override
@@ -10,151 +11,161 @@ class Questionario2Screen extends StatefulWidget {
 class _Questionario2ScreenState extends State<Questionario2Screen> {
   String selectedBloodType = 'A+';
   String selectedSexo = 'M'; // Default gender
-  final List<String> bloodTypeOptions = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
+  final List<String> bloodTypeOptions = [
+    'A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'
+  ];
   final List<String> sexoOptions = ['M', 'F'];
+  final TextEditingController ageController = TextEditingController();
+  final TextEditingController weightController = TextEditingController();
+  final TextEditingController lastDonationController = TextEditingController();
+
+  bool isEligibleToDonate() {
+    // Verifique a idade
+    final int? age = int.tryParse(ageController.text);
+    if (age == null || age < 18) {
+      return false;
+    }
+
+    // Verifique o peso
+    final int? weight = int.tryParse(weightController.text);
+    if (weight == null || weight < 50) {
+      return false;
+    }
+
+    return true;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Questionário de Elegibilidade'),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Idade',
-                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20.0),
+              Text(
+                'Idade',
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              ),
+              TextFormField(
+                controller: ageController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: 'Digite sua idade',
+                  border: OutlineInputBorder(),
                 ),
-                TextFormField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    hintText: 'Digite sua idade',
-                  ),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Peso',
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              ),
+              TextFormField(
+                controller: weightController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: 'Digite seu peso em kg',
+                  border: OutlineInputBorder(),
                 ),
-                SizedBox(height: 20.0),
-                Text(
-                  'Peso',
-                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Tipo Sanguíneo',
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              ),
+              DropdownButtonFormField<String>(
+                value: selectedBloodType,
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedBloodType = newValue!;
+                  });
+                },
+                items: bloodTypeOptions.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
                 ),
-                TextFormField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    hintText: 'Digite seu peso',
-                  ),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Data da Última Doação',
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              ),
+              TextFormField(
+                controller: lastDonationController,
+                keyboardType: TextInputType.datetime,
+                decoration: InputDecoration(
+                  hintText: 'DD/MM/AAAA',
+                  border: OutlineInputBorder(),
                 ),
-                SizedBox(height: 20.0),
-                Text(
-                  'Tipo Sanguíneo',
-                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                'Sexo',
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              ),
+              DropdownButtonFormField<String>(
+                value: selectedSexo,
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedSexo = newValue!;
+                  });
+                },
+                items: sexoOptions.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
                 ),
-                DropdownButton<String>(
-                  value: selectedBloodType,
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      selectedBloodType = newValue!;
-                    });
-                  },
-                  items: bloodTypeOptions.map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                ),
-                SizedBox(height: 20.0),
-                Text(
-                  'Data da Última Doação',
-                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                ),
-                TextFormField(
-                  inputFormatters: [
-                    LengthLimitingTextInputFormatter(8),
-                    FilteringTextInputFormatter.allow(RegExp(r'^\d*')),
-                    _DateInputFormatter(),
-                  ],
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    hintText: 'DD/MM/YY',
-                  ),
-                ),
-                SizedBox(height: 20.0),
-                Text(
-                  'Sexo',
-                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                ),
-                DropdownButton<String>(
-                  value: selectedSexo,
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      selectedSexo = newValue!;
-                    });
-                  },
-                  items: sexoOptions.map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                ),
-              ],
-            ),
-            Positioned(
-              bottom: 6.0,
-              child: Container(
-                width: 360.0,
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Navigate to Questionario3Screen
+              ),
+              SizedBox(height: 40.0),
+              ElevatedButton(
+                onPressed: () {
+                  if (isEligibleToDonate()) {
                     Navigator.push(
                       context,
                       MaterialPageRoute(builder: (context) => Questionario3Screen()),
                     );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 255, 17, 0),
-                    padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
+                  } else {
+                    // Se não for elegível, navegue para a tela de inelegibilidade
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Questionario4Screen()),
+                    );
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red,
+                  onPrimary: Colors.white,
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  child: Text(
-                    'Verificar',
-                    style: TextStyle(
-                      fontSize: 22.0,
-                      color: Colors.white,
-                    ),
-                  ),
+                  minimumSize: Size(double.infinity, 50),
+                ),
+                child: Text(
+                  'Próximo',
+                  style: TextStyle(fontSize: 18),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
-  }
-}
-
-class _DateInputFormatter extends TextInputFormatter {
-  @override
-  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
-    final newText = _formatDate(newValue.text);
-    return TextEditingValue(
-      text: newText,
-      selection: TextSelection.collapsed(offset: newText.length),
-    );
-  }
-
-  String _formatDate(String text) {
-    text = text.replaceAll(RegExp(r'[^0-9]'), '');
-    if (text.length > 4) {
-      text = text.substring(0, 2) + '/' + text.substring(2, 4) + '/' + text.substring(4);
-    }
-    return text;
   }
 }
--- sangueconecta/lib/questionario4.dart
+++ sangueconecta/lib/questionario4.dart
@@ -1,62 +1,55 @@
 import 'package:flutter/material.dart';
+import 'noticias.dart'; // Assegure-se de que o import esteja correto
 
 class Questionario4Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('Status da Elegibilidade'),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+      ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
               'Infelizmente você não está apto a doar.',
+              textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 32.0,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 50.0),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SizedBox(height: 20.0),
-                  Text(
-                    'Mas calma, aproveite e olhe nossos conteudos informativos e compartilhe com seus amigos e familiares.',
-                    style: TextStyle(
-                      fontSize: 22.0,
-                    ),
-                  ),
-                  SizedBox(height: 20.0),
-                  Positioned(
-                    bottom: 6.0,
-                    child: Container(
-                      width: 360.0,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          print("continuar pressionado");
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor:
-                              const Color.fromARGB(255, 255, 17, 0),
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 20.0, vertical: 40.0),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12.0),
-                          ),
-                        ),
-                        child: Text(
-                          'Continuar',
-                          style: TextStyle(
-                            fontSize: 22.0,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+            SizedBox(height: 24.0),
+            Text(
+              'Mas calma, aproveite e olhe nossos conteúdos informativos e compartilhe com seus amigos e familiares.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 22.0,
+              ),
+            ),
+            SizedBox(height: 48.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => NewsScreen()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red, // Cor de fundo do botão
+                onPrimary: Colors.white, // Cor do texto do botão
+                padding: EdgeInsets.symmetric(vertical: 16),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10), // Borda arredondada
+                ),
+                minimumSize: Size(double.infinity, 50), // Tamanho mínimo
+              ),
+              child: Text(
+                'Ver Notícias',
+                style: TextStyle(fontSize: 18),
               ),
             ),
           ],
